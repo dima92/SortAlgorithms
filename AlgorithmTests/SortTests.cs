d29eafda3d79d02e764cc33055392818797693e5
Дмитрий <dima-tkachenko@mail.ru>
2020-01-18 20:52:45 +0300

    Добавлена сортировка вставками

---
+++ AlgorithmTests/SortTests.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Algorithm.Tests
+{
+    [TestClass()]
+    public class SortTests
+    {
+        readonly Random rnd = new Random();
+        readonly List<int> Items = new List<int>();
+        readonly List<int> Sorted = new List<int>();
+
+        [TestInitialize]
+        public void Init()
+        {
+            Items.Clear();
+            for (int i = 0; i < 10000; i++)
+            {
+                Items.Add(rnd.Next(0, 1000));
+            }
+
+            Sorted.Clear();
+            Sorted.AddRange(Items.OrderBy(x => x).ToArray());
+        }
+
+        [TestMethod()]
+        public void BubbleSortTest()
+        {
+            // arrange
+            var bubble = new BubbleSort<int>();
+            bubble.Items.AddRange(Items);
+
+            // act
+            bubble.Sort();
+
+            // assert
+            for (int i = 0; i < Items.Count; i++)
+            {
+                Assert.AreEqual(Sorted[i], bubble.Items[i]);
+            }
+        }
+
+        [TestMethod()]
+        public void CocktailSortTest()
+        {
+            // arrange
+            var cocktail = new CoctailSort<int>();
+            cocktail.Items.AddRange(Items);
+
+            // act
+            cocktail.Sort();
+
+            // assert
+            for (int i = 0; i < Items.Count; i++)
+            {
+                Assert.AreEqual(Sorted[i], cocktail.Items[i]);
+            }
+        }
+
+        [TestMethod()]
+        public void InsertSortTest()
+        {
+            // arrange
+            var insert = new InsertSort<int>();
+            insert.Items.AddRange(Items);
+
+            // act
+            insert.Sort();
+
+            // assert
+            for (int i = 0; i < Items.Count; i++)
+            {
+                Assert.AreEqual(Sorted[i], insert.Items[i]);
+            }
+        }
+    }
+}
