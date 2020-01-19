c978c8a92e51f2f2a973a011b4a9d2cb01d152f4
Дмитрий <dima-tkachenko@mail.ru>
2020-01-19 20:00:18 +0300

    Реализована пирамидальная сортировка(двоичная куча)

---
+++ Algorithm/HeapSort.cs
@@ -0,0 +1,15 @@
+﻿using Algorithm.DataStructures;
+using System;
+
+namespace Algorithm
+{
+    public class HeapSort<T> : AlgorithmBase<T> where T : IComparable
+    {
+        protected override void MakeSort()
+        {
+            var heap = new Heap<T>(Items);
+            var sorted = heap.Order();
+            Items = sorted;
+        }
+    }
+}
